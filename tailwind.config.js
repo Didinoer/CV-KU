80bde2ecd89e0b8639fd10b812cb40d4ea896a08
Didi Nurahman <88863426+Didinoer@users.noreply.github.com>
2022-07-15 16:22:43 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        '2P' : "'Press Start 2P', cursive"
+      },
+    },
+  },
+  plugins: [],
+}
